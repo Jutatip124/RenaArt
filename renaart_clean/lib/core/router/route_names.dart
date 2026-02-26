ea9b538b773682ce3373619dc45e15ef04a35cdc
Jutatip <6631503124@lamduan.mfu.ac.th>
2026-02-26 23:30:24 +0700

    Delete renaart_clean directory

--- renaart_clean/lib/core/router/route_names.dart
+++
@@ -1,9 +0,0 @@
-class RouteNames {
-  static const home = '/';
-  static const search = '/search';
-  static const collection = '/collection';
-  static const profile = '/profile';
-  static const artworkDetail = '/artwork/:id';
-
-  static String artworkDetailPath(String id) => '/artwork/$id';
-}
