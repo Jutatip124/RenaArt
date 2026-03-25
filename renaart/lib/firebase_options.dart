e48e4737f3a8193c3d1df67c0f5a0f77ab76c43b
Jutatip <6631503124@lamduan.mfu.ac.th>
2026-03-25 12:07:14 +0000

    Fixdotenv

--- renaart/lib/firebase_options.dart
+++ renaart/lib/firebase_options.dart
@@ -3,6 +3,7 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 /// Default [FirebaseOptions] for use with your Firebase apps.
 ///
@@ -52,13 +53,13 @@ class DefaultFirebaseOptions {
     }
   }
 
-  static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyCoBUU9mGWjnbqM3idEjL1znOJtCn5zZAI',
-    appId: '1:482100656439:web:da0710ef0a7324699042b2',
-    messagingSenderId: '482100656439',
-    projectId: 'renaart-ded29',
-    authDomain: 'renaart-ded29.firebaseapp.com',
-    storageBucket: 'renaart-ded29.firebasestorage.app',
-    measurementId: 'G-VSP6EB3YFV',
-  );
+  static FirebaseOptions get web => FirebaseOptions(
+        apiKey: dotenv.env['FIREBASE_WEB_API_KEY'] ?? '',
+        appId: dotenv.env['FIREBASE_WEB_APP_ID'] ?? '',
+        messagingSenderId: dotenv.env['FIREBASE_WEB_MESSAGING_SENDER_ID'] ?? '',
+        projectId: dotenv.env['FIREBASE_WEB_PROJECT_ID'] ?? '',
+        authDomain: dotenv.env['FIREBASE_WEB_AUTH_DOMAIN'] ?? '',
+        storageBucket: dotenv.env['FIREBASE_WEB_STORAGE_BUCKET'] ?? '',
+        measurementId: dotenv.env['FIREBASE_WEB_MEASUREMENT_ID'] ?? '',
+      );
 }
--- renaart/lib/main.dart
+++ renaart/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'firebase_options.dart';
 import 'core/theme/app_theme.dart';
 import 'core/router/app_router.dart';
@@ -12,6 +13,13 @@ import 'services/local_storage_service.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Load environment variables from .env
+  try {
+    await dotenv.load();
+  } catch (e) {
+    debugPrint('Error loading .env: $e');
+  }
 
   // Initialize Firebase
   try {
